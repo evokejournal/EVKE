40483b2077be5cfce18f884a33db10fd8d054b6b
evokejournal <evoke.theapp@gmail.com>
2025-06-01 11:37:40 +1000

    2

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,14 +5,14 @@ import { getDatabase } from "firebase/database"
 import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyDfVp5WDEFfqUibog9xE-jRwNLExiplsP0",
+  authDomain: "evke-f6fed.firebaseapp.com",
+  databaseURL: "https://evke-f6fed-default-rtdb.firebaseio.com",
+  projectId: "evke-f6fed",
+  storageBucket: "evke-f6fed.firebasestorage.app",
+  messagingSenderId: "477445312641",
+  appId: "1:477445312641:web:13f343e5e5942244e2fd80",
+  measurementId: "G-K5YZT6KPFM"
 }
 
 // Initialize Firebase
